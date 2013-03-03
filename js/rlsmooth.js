2b8d42cad8837e454f97a94e7503df2d403324d0
Rodrigo-Ludgero <rludgero@yahoo.es>
2013-03-03 23:29:07 +0100

    indentation

--- js/rlsmooth.js
+++ js/rlsmooth.js
@@ -14,27 +14,27 @@
 
 $.fn.rlSmooth = function(options) {
 
- 		var settings = $.extend({
-	    y: 0, //ms default settings for vertical window position
-	    down: 500, // ms default settings for slide down effect speed
-	    up: 500 // ms default settings for slide up effect speed
-    }, options);
+		var settings = $.extend({
+			y: 0, //ms default settings for vertical window position
+			down: 500, // ms default settings for slide down effect speed
+			up: 500 // ms default settings for slide up effect speed
+		}, options);
 
 
- 		return this.each(function() {
+		return this.each(function() {
 
 			var $this = $(this); // global object reference
 
- 		  $(window).scroll(function() {
- 		    if ($(window).scrollTop() > settings.y) {
- 		      $this.slideDown(settings.down);
- 		    }
- 		    else {
- 		      $this.slideUp(settings.up);
- 		    }
- 		  });
- 	  });
+			$(window).scroll(function() {
+				if ($(window).scrollTop() > settings.y) {
+					$this.slideDown(settings.down);
+				}
+				else {
+					$this.slideUp(settings.up);
+				}
+			});
+		});
 
- 	};
+	};
 
 })( jQuery );
